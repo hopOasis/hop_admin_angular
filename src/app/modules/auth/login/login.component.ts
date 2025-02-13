3df0556ee9096d5b16c1acf78bb484a0502507b7
SemyonTrushikQQ <semen.trushik.gh@gmail.com>
2025-02-14 01:20:31 +0200

    added login page and project structure

---
+++ src/app/modules/auth/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+    ])
+  });
+
+  hidePassword = true;
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Форма валидна', this.loginForm.value);
+      this.loginForm.reset();
+    } else {
+      alert('Ошибка: Проверьте корректность email и пароля.');
+    }
+  }
+}
