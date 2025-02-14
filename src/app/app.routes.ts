4d004de2ed4fcff50a2525ece4009cd740c58897
SemyonTrushikQQ <semen.trushik.gh@gmail.com>
2025-02-14 03:52:37 +0200

    add main-layout update app.components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './core/guard/guard/guard.component'; 
+import { AuthGuard } from './core/guard/guard.component';
+import { LayoutComponent } from './modules/layout/layout/layout.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-
   { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
-
-  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
-  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule), canActivate: [AuthGuard] },
-  { path: 'orders', loadChildren: () => import('./modules/orders/orders.module').then(m => m.OrdersModule), canActivate: [AuthGuard] },
-  { path: 'reviews', loadChildren: () => import('./modules/reviews/reviews.module').then(m => m.ReviewsModule), canActivate: [AuthGuard] },
-
+  {
+    path: '',
+    component: LayoutComponent, 
+    canActivate: [AuthGuard], 
+    children: [
+      { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+      { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) },
+      { path: 'orders', loadChildren: () => import('./modules/orders/orders.module').then(m => m.OrdersModule) },
+      { path: 'reviews', loadChildren: () => import('./modules/reviews/reviews.module').then(m => m.ReviewsModule) }
+    ]
+  },
   { path: '**', redirectTo: 'auth/login' } 
 ];
 
