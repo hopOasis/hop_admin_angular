598460aa629e4888718310d7d9524c6ae7f73d34
SemyonTrushikQQ <semen.trushik.gh@gmail.com>
2025-02-17 17:37:12 +0200

    add logout-logic

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { TokenService } from '../../core/services/token/token.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,16 +16,27 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule, 
     MatListModule, 
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    
   ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  constructor(
+    private router: Router,
+    private tokenService: TokenService,
+  ) {
+  }
   links = [
     { path: 'dashboard', label: 'Dashboard', icon: 'space_dashboard' },
     { path: 'products', label: 'Products', icon: 'inventory_2' },
     { path: 'orders', label: 'Orders', icon: 'fact_check' },
     { path: 'reviews', label: 'Reviews', icon: 'feedback' }
   ];
+  logout() {
+    this.tokenService.removeToken()
+    this.router.navigate(['']);
+  }
+
 }
